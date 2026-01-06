e7e5d2d0ea679b9b6f117f4f40118a198fe3caa4
YOUR_NAME <YOUR_EMAIL>
2026-01-06 12:18:25 +0200

    Build: ensure pluginManagement repos (gradlePluginPortal/google/mavenCentral)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,11 @@ pluginManagement {
     repositories {
         
         
+        
         gradlePluginPortal()
+        google()
+        mavenCentral()
+gradlePluginPortal()
         google()
         mavenCentral()
                 maven("https://artifactory.appodeal.com/appodeal-public")
@@ -12,9 +16,14 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://artifactory.appodeal.com/appodeal-public") }
         
         
 
+
+        google()
+        mavenCentral()
 maven(url = "https://jitpack.io")
                 maven("https://artifactory.appodeal.com/appodeal-public")
 }
